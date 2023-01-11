abfd45c3a9fa48e3279f9a88c7b307a2a15a3290
Rayncor <sowa332@gmail.com>
2023-01-11 11:07:08 -0800

    a little practice triggering useeffects mounts

--- src/components/StatePractice.jsx
+++ src/components/StatePractice.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const StatePractice = () => {
 	const [age, setAge] = useState(0);
 	const [text, setText] = useState("");
 	const [header, setHeader] = useState(false);
 	const [headerColor, setHeaderColor] = useState("red");
+
+	//good place to put an intiial api call- will fetch data immediately
+	//final array tells us what states we want to trigger the useeffect
+
+	useEffect(() => {
+		console.log("component mounted");
+
+		//return function is executed when component unmounts
+		return () => {
+			console.log("component unmounted");
+		};
+	}, [text, header]);
 
 	const increaseAge = () => {
 		setAge((Math.floor(age) * 7) / 3 + 1).float();
@@ -14,6 +26,12 @@ export const StatePractice = () => {
 		console.log(event.target.value);
 		setText(event.target.value);
 	};
+
+	//component lifecycle: mounting stage, updating stage, unmounting stage
+	//mounting: component starting to exist
+	//unmounting: component ceasing to exist
+	//useeffect is a hook that does something depending on what stage of the lifecycle your page is in
+	//i.e. actions depending on whether component is mounting, unmounting, updating, etc
 
 	return (
 		<div>
